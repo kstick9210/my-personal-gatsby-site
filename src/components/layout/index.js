91ea3a92d999d3b808487698e52afd0de5578ec0
Kathleen Stickel <kstick9210@outlook.com>
2020-10-02 18:27:58 -0500

    update layout component

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import Header from '../header'
+import Footer from '../footer'
 
 export default (props) => (
     <div>
-        
+        <Header />
+        <main>
+            {props.children}
+        </main>
+        <Footer />
     </div>
 )
