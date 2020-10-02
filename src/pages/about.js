19eaa72a7f3433af28af0a9916823a74f3e05d36
Kathleen Stickel <kstick9210@outlook.com>
2020-10-02 18:18:01 -0500

    create header

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 export default function About() {
     return (
         <div>
             <h1>About Me</h1>
             <p>I love React</p>
+            <Link to="/contact">Contact</Link>
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from "react"
+import { Link } from 'gatsby'
 
 export default function Home() {
   return (
     <div>
       <h1>KMS</h1>
+      <Link to="/about">About me</Link><br></br>
+      <Link to="/contact">Contact me</Link>
     </div>
   )
 }
