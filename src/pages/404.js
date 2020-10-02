0f514402f33855dd652968ee922f6d2b6f3331bc
Kathleen Stickel <kstick9210@outlook.com>
2020-10-02 18:03:03 -0500

    scaffold app

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+export default (props) => (
+    <div>
+        <h1>Whoops!</h1>
+        <h2>That page doesn't seem to be here</h2>
+        <Link to="/">Go back to the home page</Link>
+    </div>
+)
---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Contact() {
+    return (
+        <div>
+            <h1>Contact Me</h1>
+            <h3>Here is how to contact me</h3>
+        </div>
+
+    )
+}
