a7242b31ab3ab741f37ee75e66c0c56b80314627
Kathleen Stickel <kstick9210@outlook.com>
2020-10-02 18:22:56 -0500

    update footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+export default (props) => (
+    <footer>
+        <p>Developed by Kathleen Stickel</p>
+        <Link to="https://github.com/kstick9210">Find me on GitHub</Link>
+    </footer>
+)
