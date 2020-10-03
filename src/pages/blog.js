d080bc4cac0277157939db835551b51bdc47cbdc
Kathleen Stickel <kstick9210@outlook.com>
2020-10-02 20:32:15 -0500

    dynamically render blogs

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,8 +1,43 @@
 import React from 'react'
+import { graphql, useStaticQuery, Link } from 'gatsby'
 import Layout from '../components/layout'
 
-export default (props) => (
-    <Layout>
-        <h1>blog</h1>
-    </Layout>
-)
+
+export default (props) => {
+
+    const data = useStaticQuery(graphql`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        fields {
+                            slug
+                        }
+                        html
+                        frontmatter {
+                            title
+                            date
+                        }
+                    }
+                }
+            }
+        }
+    `)
+
+    const { edges } = data.allMarkdownRemark
+
+    const posts = edges.map(({ node }) => 
+        <section key={node.fields.slug}>
+            <Link to={node.fields.slug}>
+                <h4>{node.frontmatter.title}</h4>
+            </Link>
+            <p>{node.frontmatter.date}</p>
+        </section>
+    )
+    return (
+        <Layout>
+            <h1>blog</h1>
+            {posts}
+        </Layout>
+    )
+}
